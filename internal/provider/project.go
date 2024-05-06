dd054516eccf1cf30982727b7b0c975566faebf0
Henry Dettmer <hdettmer@meshcloud.io>
2024-05-06 10:08:01 +0200

    feat: project data source

---
+++ internal/provider/project.go
@@ -0,0 +1,21 @@
+package provider
+
+type MeshProject struct {
+	ApiVersion string              `json:"apiVersion" tfsdk:"api_version"`
+	Kind       string              `json:"kind" tfsdk:"kind"`
+	Metadata   MeshProjectMetadata `json:"metadata" tfsdk:"metadata"`
+	Spec       MeshProjectSpec     `json:"spec" tfsdk:"spec"`
+}
+type MeshProjectMetadata struct {
+	Name             string  `json:"name" tfsdk:"name"`
+	OwnedByWorkspace string  `json:"ownedByWorkspace" tfsdk:"owned_by_workspace"`
+	CreatedOn        string  `json:"createdOn" tfsdk:"created_on"`
+	DeletedOn        *string `json:"deletedOn" tfsdk:"deleted_on"`
+}
+
+type MeshProjectSpec struct {
+	DisplayName                       string              `json:"displayName" tfsdk:"display_name"`
+	Tags                              map[string][]string `json:"tags" tfsdk:"tags"`
+	PaymentMethodIdentifier           string              `json:"paymentMethodIdentifier" tfsdk:"payment_method_identifier"`
+	SubstitutePaymentMethodIdentifier string              `json:"substitutePaymentMethodIdentifier" tfsdk:"substitute_payment_method_identifier"`
+}
---
+++ internal/provider/project_data_source.go
@@ -0,0 +1,115 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var (
+	_ datasource.DataSource              = &projectDataSource{}
+	_ datasource.DataSourceWithConfigure = &projectDataSource{}
+)
+
+func NewProjectDataSource() datasource.DataSource {
+	return &projectDataSource{}
+}
+
+type projectDataSource struct {
+	client *MeshStackProviderClient
+}
+
+func (d *projectDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_project"
+}
+
+func (d *projectDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Single project by name and workspace.",
+
+		Attributes: map[string]schema.Attribute{
+			"api_version": schema.StringAttribute{
+				MarkdownDescription: "Project datatype version",
+				Computed:            true,
+			},
+
+			"kind": schema.StringAttribute{
+				MarkdownDescription: "meshObject type, always `meshBuildingBlock`.",
+				Computed:            true,
+				Validators: []validator.String{
+					stringvalidator.OneOf([]string{"meshProject"}...),
+				},
+			},
+
+			"metadata": schema.SingleNestedAttribute{
+				MarkdownDescription: "Project metadata. Name and workspace of the target Project must be set here.",
+				Required:            true,
+				Attributes: map[string]schema.Attribute{
+					"name":               schema.StringAttribute{Required: true},
+					"owned_by_workspace": schema.StringAttribute{Required: true},
+					"created_on":         schema.StringAttribute{Computed: true},
+					"deleted_on":         schema.StringAttribute{Computed: true},
+				},
+			},
+
+			"spec": schema.SingleNestedAttribute{
+				MarkdownDescription: "Project specification.",
+				Computed:            true,
+				Attributes: map[string]schema.Attribute{
+
+					"display_name": schema.StringAttribute{Computed: true},
+					"tags": schema.MapAttribute{
+						ElementType: types.ListType{ElemType: types.StringType},
+						Computed:    true,
+					},
+					"payment_method_identifier":            schema.StringAttribute{Computed: true},
+					"substitute_payment_method_identifier": schema.StringAttribute{Computed: true},
+				},
+			},
+		},
+	}
+}
+
+func (d *projectDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*MeshStackProviderClient)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *MeshStackProviderClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+}
+
+func (d *projectDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	// get workspace and name to query for project
+	var workspace string
+	resp.Diagnostics.Append(req.Config.GetAttribute(ctx, path.Root("metadata").AtName("owned_by_workspace"), &workspace)...)
+
+	var name string
+	resp.Diagnostics.Append(req.Config.GetAttribute(ctx, path.Root("metadata").AtName("name"), &name)...)
+
+	project, err := d.client.ReadProject(workspace, name)
+	if err != nil {
+		resp.Diagnostics.AddError("Unable to read project", err.Error())
+	}
+
+	// client data maps directly to the schema so we just need to set the state
+	resp.Diagnostics.Append(resp.State.Set(ctx, project)...)
+	return
+}
