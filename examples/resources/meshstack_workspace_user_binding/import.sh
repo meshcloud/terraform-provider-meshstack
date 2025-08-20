5ca393a12a239ff1af98645623facb41c2772786
OliverEsoterik <oliver.esoterik@gmail.com>
2025-08-20 11:57:38 +0200

    change project to workspace user binding in examples

--- examples/resources/meshstack_workspace_user_binding/import.sh
+++ examples/resources/meshstack_workspace_user_binding/import.sh
@@ -1,2 +1,2 @@
 # import via workspace binding name
-terraform import 'meshstack_project_user_binding.example' 'my-binding-name'
+terraform import 'meshstack_workspace_user_binding.example' 'my-binding-name'
