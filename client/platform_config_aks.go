0b717b0d8d4473eba0bd51005b48de5481005ad6
Henry Dettmer <hdettmer@meshcloud.io>
2025-11-17 14:43:44 +0100

    refactor: AksServicePrincipal

--- client/platform_config_aks.go
+++ client/platform_config_aks.go
@@ -8,20 +8,20 @@ type AksPlatformConfig struct {
 }
 
 type AksReplicationConfig struct {
-	AccessToken             string                 `json:"accessToken" tfsdk:"access_token"`
-	NamespaceNamePattern    string                 `json:"namespaceNamePattern" tfsdk:"namespace_name_pattern"`
-	GroupNamePattern        string                 `json:"groupNamePattern" tfsdk:"group_name_pattern"`
-	ServicePrincipal        ServicePrincipalConfig `json:"servicePrincipal" tfsdk:"service_principal"`
-	AksSubscriptionId       string                 `json:"aksSubscriptionId" tfsdk:"aks_subscription_id"`
-	AksClusterName          string                 `json:"aksClusterName" tfsdk:"aks_cluster_name"`
-	AksResourceGroup        string                 `json:"aksResourceGroup" tfsdk:"aks_resource_group"`
-	RedirectUrl             *string                `json:"redirectUrl,omitempty" tfsdk:"redirect_url"`
-	SendAzureInvitationMail bool                   `json:"sendAzureInvitationMail" tfsdk:"send_azure_invitation_mail"`
-	UserLookUpStrategy      string                 `json:"userLookUpStrategy" tfsdk:"user_look_up_strategy"`
-	AdministrativeUnitId    *string                `json:"administrativeUnitId,omitempty" tfsdk:"administrative_unit_id"`
+	AccessToken             string                    `json:"accessToken" tfsdk:"access_token"`
+	NamespaceNamePattern    string                    `json:"namespaceNamePattern" tfsdk:"namespace_name_pattern"`
+	GroupNamePattern        string                    `json:"groupNamePattern" tfsdk:"group_name_pattern"`
+	ServicePrincipal        AksServicePrincipalConfig `json:"servicePrincipal" tfsdk:"service_principal"`
+	AksSubscriptionId       string                    `json:"aksSubscriptionId" tfsdk:"aks_subscription_id"`
+	AksClusterName          string                    `json:"aksClusterName" tfsdk:"aks_cluster_name"`
+	AksResourceGroup        string                    `json:"aksResourceGroup" tfsdk:"aks_resource_group"`
+	RedirectUrl             *string                   `json:"redirectUrl,omitempty" tfsdk:"redirect_url"`
+	SendAzureInvitationMail bool                      `json:"sendAzureInvitationMail" tfsdk:"send_azure_invitation_mail"`
+	UserLookUpStrategy      string                    `json:"userLookUpStrategy" tfsdk:"user_look_up_strategy"`
+	AdministrativeUnitId    *string                   `json:"administrativeUnitId,omitempty" tfsdk:"administrative_unit_id"`
 }
 
-type ServicePrincipalConfig struct {
+type AksServicePrincipalConfig struct {
 	ClientId                    string  `json:"clientId" tfsdk:"client_id"`
 	AuthType                    string  `json:"authType" tfsdk:"auth_type"`
 	CredentialsAuthClientSecret *string `json:"credentialsAuthClientSecret,omitempty" tfsdk:"credentials_auth_client_secret"`
