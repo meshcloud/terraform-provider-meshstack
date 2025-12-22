336d1f1429f75cce4566e3aa33b9fb8250fc98b6
Andreas Grub <agrub@meshcloud.io>
2025-12-22 16:31:18 +0100

    feat: add first acceptance test for meshstack_location, runs locally

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,24 +1,28 @@
 package provider
 
 import (
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/stretchr/testify/require"
 )
 
 // testAccProtoV6ProviderFactories are used to instantiate a provider during
 // acceptance testing. The factory function will be invoked for every Terraform
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
-// nolint: unused
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"meshstack": providerserver.NewProtocol6WithError(New("test")()),
 }
 
-// nolint: unused
 func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
+	t.Helper()
+	endpoint := os.Getenv(envKeyMeshstackEndpoint)
+	require.Truef(t, strings.HasPrefix(endpoint, "http://localhost"),
+		"Env %s='%s' does not start with http://localhost, only locally running meshStacks should be used for tests", envKeyMeshstackEndpoint, endpoint)
+	require.NotEmptyf(t, os.Getenv(envKeyMeshstackApiKey), "Env %s empty, please set before running", envKeyMeshstackApiKey)
+	require.NotEmptyf(t, os.Getenv(envKeyMeshstackApiSecret), "Env %s empty, please set before running", envKeyMeshstackApiSecret)
 }
