d03f673ae274ad519eff922e64412a89bb388f3b
Andreas Grub <agrub@meshcloud.io>
2025-12-22 16:31:18 +0100

    feat: add first acceptance test for meshstack_location, runs locally

---
+++ internal/provider/location_resource_test.go
@@ -0,0 +1,58 @@
+package provider
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/meshcloud/terraform-provider-meshstack/examples"
+)
+
+func TestAccLocation(t *testing.T) {
+	const resourceAddress = "meshstack_location.example"
+	const resourceIdentifier = "my-location"
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		PreCheck:                 func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: examples.LocationResourceConfig,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceAddress, plancheck.ResourceActionCreate),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("metadata").AtMapKey("name"), knownvalue.StringExact(resourceIdentifier)),
+					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("spec").AtMapKey("display_name"), knownvalue.StringExact("My Cloud Location")),
+					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("spec").AtMapKey("description"), knownvalue.StringExact("A location for managing cloud resources")),
+					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("status").AtMapKey("is_public"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("ref").AtMapKey("kind"), knownvalue.StringExact("meshLocation")),
+					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("ref").AtMapKey("name"), knownvalue.StringExact(resourceIdentifier)),
+				},
+			},
+			{
+				Config: strings.ReplaceAll(examples.LocationResourceConfig, `"My Cloud Location"`, `"My Updated Location"`),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(resourceAddress, plancheck.ResourceActionUpdate),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("spec").AtMapKey("display_name"), knownvalue.StringExact("My Updated Location")),
+				},
+			},
+			{
+				ImportState:     true,
+				ImportStateKind: resource.ImportBlockWithID,
+				ImportStateId:   resourceIdentifier,
+				ResourceName:    resourceAddress,
+			},
+		},
+	})
+}
