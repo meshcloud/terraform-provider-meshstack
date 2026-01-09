181fe46fc217c1f8a43716eccf01c898edc03819
Andreas Grub <agrub@meshcloud.io>
2026-01-09 16:34:37 +0100

    refactor: simplify fetching paginated responses, add simplistic data source test

--- examples/embed.go
+++ examples/embed.go
@@ -5,4 +5,8 @@ import _ "embed"
 var (
 	//go:embed resources/meshstack_location/resource.tf
 	LocationResourceConfig string
+	//go:embed data-sources/meshstack_projects/data-source_all.tf
+	ProjectsDataSourceConfig string
+	//go:embed data-sources/meshstack_projects/data-source_payment_method.tf
+	ProjectsWithPaymentMethodDataSourceConfig string
 )
