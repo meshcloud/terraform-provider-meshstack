cd5639be8b5a6ec040cb24df76b0b4c15b213027
Andreas Grub <agrub@meshcloud.io>
2026-02-04 11:48:03 +0100

    feat: add mock client and extend resource test for meshstack_location

--- internal/provider/location_resource_test.go
+++ internal/provider/location_resource_test.go
@@ -2,7 +2,6 @@ package provider
 
 import (
 	_ "embed"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -10,49 +9,95 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/meshcloud/terraform-provider-meshstack/examples"
+	"github.com/meshcloud/terraform-provider-meshstack/internal/clientmock"
 )
 
 func TestAccLocation(t *testing.T) {
-	const resourceAddress = "meshstack_location.example"
+	runLocationResourceTestCase(t)
+}
+
+func TestLocation(t *testing.T) {
+	// Run acceptance tests as unit tests with mock
+	runLocationResourceTestCase(t, SetupMockClient(func(t *testing.T, testCase *resource.TestCase, mockClient clientmock.Client) {
+		t.Helper()
+		testCase.Steps[0].PostApplyFunc = func() {
+			assert.Len(t, mockClient.Location.Store, 1)
+		}
+	}))
+}
+
+func runLocationResourceTestCase(t *testing.T, modifiers ...ResourceTestCaseModifier) {
+	t.Helper()
+	var resourceAddress examples.Identifier
+	config := examples.Resource{Name: "location"}.Config().
+		SingleResourceAddress(&resourceAddress)
+
 	const resourceIdentifier = "my-location"
-	resource.Test(t, resource.TestCase{
-		ProtoV6ProviderFactories: ProviderFactoriesForTest(),
-		PreCheck:                 func() { DefaultTestPreCheck(t) },
+
+	testCase := resource.TestCase{
 		Steps: []resource.TestStep{
 			{
-				Config: examples.Resource{Name: "location"}.Config().String(),
+				Config: config.String(),
 				ConfigPlanChecks: resource.ConfigPlanChecks{
 					PreApply: []plancheck.PlanCheck{
-						plancheck.ExpectResourceAction(resourceAddress, plancheck.ResourceActionCreate),
+						plancheck.ExpectResourceAction(resourceAddress.String(), plancheck.ResourceActionCreate),
 					},
 				},
 				ConfigStateChecks: []statecheck.StateCheck{
-					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("metadata").AtMapKey("name"), knownvalue.StringExact(resourceIdentifier)),
-					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("spec").AtMapKey("display_name"), knownvalue.StringExact("My Cloud Location")),
-					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("spec").AtMapKey("description"), knownvalue.StringExact("A location for managing cloud resources")),
-					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("status").AtMapKey("is_public"), knownvalue.Bool(false)),
-					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("ref").AtMapKey("name"), knownvalue.StringExact(resourceIdentifier)),
+					statecheck.ExpectKnownValue(resourceAddress.String(), tfjsonpath.New("metadata"), checkLocationMetadata(resourceIdentifier)),
+					statecheck.ExpectKnownValue(resourceAddress.String(), tfjsonpath.New("spec"), checkLocationSpec("My Cloud Location")),
+					statecheck.ExpectKnownValue(resourceAddress.String(), tfjsonpath.New("status"), checkLocationStatus()),
+					statecheck.ExpectKnownValue(resourceAddress.String(), tfjsonpath.New("ref"), checkLocationRef(resourceIdentifier)),
 				},
 			},
 			{
-				Config: strings.ReplaceAll(examples.Resource{Name: "location"}.Config().String(), `"My Cloud Location"`, `"My Updated Location"`),
+				Config: config.ReplaceAll(`"My Cloud Location"`, `"My Updated Location"`).String(),
 				ConfigPlanChecks: resource.ConfigPlanChecks{
 					PreApply: []plancheck.PlanCheck{
-						plancheck.ExpectResourceAction(resourceAddress, plancheck.ResourceActionUpdate),
+						plancheck.ExpectResourceAction(resourceAddress.String(), plancheck.ResourceActionUpdate),
 					},
 				},
 				ConfigStateChecks: []statecheck.StateCheck{
-					statecheck.ExpectKnownValue(resourceAddress, tfjsonpath.New("spec").AtMapKey("display_name"), knownvalue.StringExact("My Updated Location")),
+					statecheck.ExpectKnownValue(resourceAddress.String(), tfjsonpath.New("spec"), checkLocationSpec("My Updated Location")),
 				},
 			},
 			{
 				ImportState:     true,
 				ImportStateKind: resource.ImportBlockWithID,
 				ImportStateId:   resourceIdentifier,
-				ResourceName:    resourceAddress,
+				ResourceName:    resourceAddress.String(),
 			},
 		},
+	}
+
+	ResourceTestCaseModifiers(modifiers).ApplyAndTest(t, testCase)
+}
+
+func checkLocationMetadata(name string) knownvalue.Check {
+	return knownvalue.MapExact(map[string]knownvalue.Check{
+		"name": knownvalue.StringExact(name),
+		"uuid": KnownValueNotEmptyString(),
+	})
+}
+
+func checkLocationSpec(displayName string) knownvalue.Check {
+	return knownvalue.MapExact(map[string]knownvalue.Check{
+		"display_name": knownvalue.StringExact(displayName),
+		"description":  knownvalue.StringExact("A location for managing cloud resources"),
+	})
+}
+
+func checkLocationStatus() knownvalue.Check {
+	return knownvalue.MapExact(map[string]knownvalue.Check{
+		"is_public": knownvalue.Bool(false),
+	})
+}
+
+func checkLocationRef(name string) knownvalue.Check {
+	return knownvalue.MapExact(map[string]knownvalue.Check{
+		"name": knownvalue.StringExact(name),
 	})
 }
