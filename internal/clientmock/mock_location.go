a4cc3951344e0a10b181f9e8f5acd5eacd163f2a
Andreas Grub <agrub@meshcloud.io>
2026-02-04 11:48:03 +0100

    feat: add mock client and extend resource test for meshstack_location

---
+++ internal/clientmock/mock_location.go
@@ -0,0 +1,51 @@
+package clientmock
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+
+	"github.com/meshcloud/terraform-provider-meshstack/client"
+)
+
+type MeshLocationClient struct {
+	Store Store[client.MeshLocation]
+}
+
+func (m MeshLocationClient) Read(_ context.Context, name string) (*client.MeshLocation, error) {
+	if location, ok := m.Store[name]; ok {
+		return location, nil
+	}
+	return nil, nil
+}
+
+func (m MeshLocationClient) Create(_ context.Context, location *client.MeshLocationCreate) (*client.MeshLocation, error) {
+	locationUuid := acctest.RandString(32)
+	created := &client.MeshLocation{
+		ApiVersion: location.ApiVersion,
+		Metadata: client.MeshLocationMetadata{
+			Name: location.Metadata.Name,
+			Uuid: locationUuid,
+		},
+		Spec: location.Spec,
+		Status: client.MeshLocationStatus{
+			IsPublic: false,
+		},
+	}
+	m.Store[created.Metadata.Name] = created
+	return created, nil
+}
+
+func (m MeshLocationClient) Update(_ context.Context, name string, location *client.MeshLocationCreate) (*client.MeshLocation, error) {
+	if existing, ok := m.Store[name]; ok {
+		existing.Spec = location.Spec
+		return existing, nil
+	}
+	return nil, fmt.Errorf("location not found: %s", name)
+}
+
+func (m MeshLocationClient) Delete(_ context.Context, name string) error {
+	delete(m.Store, name)
+	return nil
+}
