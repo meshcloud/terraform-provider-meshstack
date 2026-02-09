fd1a619fa60983d9418c6ba0e17a4b0e91718161
Henry Dettmer <hdettmer@meshcloud.io>
2026-02-09 11:56:32 +0100

    docs: add service instance data source examples

---
+++ examples/data-sources/meshstack_service_instance/data-source.tf
@@ -0,0 +1,5 @@
+data "meshstack_service_instance" "example" {
+  metadata = {
+    instance_id = "my-service-instance-id"
+  }
+}
---
+++ examples/data-sources/meshstack_service_instances/data-source.tf
@@ -0,0 +1,8 @@
+data "meshstack_service_instances" "all" {
+  # optional filtering
+  # workspace_identifier   = "my-workspace"
+  # project_identifier     = "my-project"
+  # marketplace_identifier = "my-marketplace"
+  # service_identifier     = "my-service"
+  # plan_identifier        = "my-plan"
+}
