e88b40e9fe930b67d5d71360bc53a371f2beb72e
Jo Schwandke <jschwandke@meshcloud.io>
2026-08-13 13:20:21 +0200

    feat: add meshStack instance data source

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -201,6 +201,7 @@ func (p *MeshStackProvider) DataSources(_ context.Context) []func() datasource.D
 		NewBuildingBlockDataSource,
 		NewBuildingBlocksDataSource,
 		NewBuildingBlockDefinitionsDataSource,
+		NewMeshStackInstanceDataSource,
 		NewProjectDataSource,
 		NewProjectsDataSource,
 		NewProjectUserBindingDataSource,
